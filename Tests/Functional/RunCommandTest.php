807597fb6c519442f03e3837c98945286a42d22e
Andreas Linden <a_linden@modix.net>
2021-02-09 09:48:50 +0100

    updated some assertContains assertions to assertStringContainsString for phpunit 9

--- Tests/Functional/RunCommandTest.php
+++ Tests/Functional/RunCommandTest.php
@@ -27,7 +27,7 @@ class RunCommandTest extends BaseTestCase
         $this->assertEquals($expectedOutput, $output);
         $this->assertEquals('failed', $a->getState());
         $this->assertEquals('', $a->getOutput());
-        $this->assertContains('Command "adoigjaoisdjfijasodifjoiajsdf" is not defined.', $a->getErrorOutput());
+        $this->assertStringContainsString('Command "adoigjaoisdjfijasodifjoiajsdf" is not defined.', $a->getErrorOutput());
         $this->assertEquals('canceled', $b->getState());
     }
 
